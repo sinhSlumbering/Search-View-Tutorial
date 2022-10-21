0fb0c5ece8805d714c5f24473b35c97bd6913c23
compu <muztoba.sinha@gmail.com>
2022-10-21 17:46:54 +0600

    added dropdown search

--- app/src/main/java/com/example/searchviewsexample/AutocompleteSearch.kt
+++ app/src/main/java/com/example/searchviewsexample/AutocompleteSearch.kt
@@ -1,11 +1,38 @@
 package com.example.searchviewsexample
 
+import android.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import com.example.searchviewsexample.databinding.ActivityAutocompleteSearchBinding
 
 class AutocompleteSearch : AppCompatActivity() {
+    //declaring the binding
+    private lateinit var binding: ActivityAutocompleteSearchBinding
+    //declaring the array to be filtered
+    val names = arrayOf("noki", "noki2", "niloy", "sinha", "not Sinha", "Srilanka")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_autocomplete_search)
+        //initializing the binding
+        binding = ActivityAutocompleteSearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //declaring the array adapter for autocompletetextview
+        var srcAdapter = ArrayAdapter(this, R.layout.simple_list_item_1, names)
+        //declaring the array adapter for listview
+        var listAdapter = ArrayAdapter(this, R.layout.simple_list_item_1, names)
+        binding.autoCompleteTextView1.threshold = 1
+        //providing the adapter to the searchview
+        binding.autoCompleteTextView1.setAdapter(srcAdapter)
+        //providing the adapter to the listview
+        binding.listView2.adapter = listAdapter
+
+        //filtering results on click of button
+        binding.searchButton.setOnClickListener {
+            val st = binding.autoCompleteTextView1.text.toString()
+            if(names.contains(st)) {
+                listAdapter.filter.filter(st)
+            }
+        }
     }
 }
