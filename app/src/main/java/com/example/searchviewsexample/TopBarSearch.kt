38a37060e6d512faf6613cb76429450a36e4672d
compu <muztoba.sinha@gmail.com>
2022-10-21 16:02:50 +0600

    added viewBinding and Initialized Activities

---
+++ app/src/main/java/com/example/searchviewsexample/TopBarSearch.kt
@@ -0,0 +1,11 @@
+package com.example.searchviewsexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TopBarSearch : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_top_bar_search)
+    }
+}
